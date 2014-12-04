aef130ecce0bd505c95f98534ae56569a7b0ceb3
btcarl <bcarlston@csumb.edu>
2014-12-04 11:48:42 -0500

    finalized lab 12

--- game.py
+++ game.py
@@ -1,26 +1,40 @@
+#CST 205 Multimedia Programming
+#Lab 12 Adventure game
+#Brian Carlston
+#David Klier
+#Phillip Powell
+#Rahel Tilahun
+
+
+#Global variables for player inventory and interactive items
 inventory =[]
 items ={}
-
+#Main function runs game
 def game():
-    printNow("*** Welcome to The CSIT Guys\' House of Horrors! ***")
-    printNow("To win the game, search for the secret in the house")
-    printNow("In each room you will be told which directions you can go")
-    printNow("You\'ll be able to go north, south, east or west by typing that direction")
-    printNow("Type help to redisplay this introduction")
-    printNow("Type exit to quit at any time")
     #constants to make code easier to read
     DIRECTION = 0
     ROOM_NAME = 1
     
-    userInput = ""
-    currentRoom = "porch"
     global inventory
     global items 
+    
+    userInput = ""
+    currentRoom = "porch"
     closetOpened = false
     inventory = []
     items = initializeItems()
     # dictionary of all rooms in the game
     rooms = initializeRooms()
+
+    printNow("*** Welcome to The CSIT Guys\' House of Horrors! ***")
+    printNow("To win the game, search for the secret in the house.")
+    printNow("In each room you will be told which directions you can go.")
+    printNow("You can go north, south, east or west by typing that direction.")
+    printNow("Type help to redisplay this introduction.")
+    printNow("Type exit to quit at any time.")
+    
+    
+
     while userInput!="exit":
         roomDescription(currentRoom)
         #get list of rooms available to travel to from current room
@@ -56,31 +70,33 @@ def game():
                 userInput = winGame()
                     
         else:
-            printNow("I don\'t understand what you are saying")
+            printNow("I do not understand what you are saying.")
         printNow("")    
-    printNow("goodbye")
+    printNow("Goodbye")
     
+#Initialize all the rooms in the game using a graph data structure to travel between rooms\nodes. Store in dictionary.   
 def initializeRooms():
-    porch = [["north","entry way"]]
+    porch = [["north","entryway"]]
     entryway = [["north","kitchen"],["south","porch"],["east","living room"]]
     diningroom = [["east","kitchen"]]
-    kitchen = [["south","entry way"],["west","dining room"]]
-    livingroom = [["north","bathroom"],["west","entry way"],["east","bedroom"]]
+    kitchen = [["south","entryway"],["west","dining room"]]
+    livingroom = [["north","bathroom"],["west","entryway"],["east","bedroom"]]
     bathroom = [["south","living room"]]
     bedroom = [["west","living room"]]
     secretroom=[["west","bedroom"]]
-    rooms = {"kitchen":kitchen, "porch":porch, "entry way":entryway, "dining room":diningroom, "living room":livingroom,"bathroom":bathroom,"bedroom":bedroom,"secret room":secretroom}
+    rooms = {"kitchen":kitchen, "porch":porch, "entryway":entryway, "dining room":diningroom, "living room":livingroom,"bathroom":bathroom,"bedroom":bedroom,"secret room":secretroom}
     return rooms
 
 def initializeItems():
     items = {"knife":"kitchen", "book":"living room"}
     return items
-               
+
+#Sanity checks and validates userInput returns modified string of action to take.               
 def parse(userInput):
-    if userInput is not None :
+    if userInput is not None and userInput!="" :
       userInput = userInput.lower()
       userInputTokens = userInput.split()
-      #error checking 
+      
       if (userInput == "north" or userInput == "south" or userInput == "east" or userInput == "west"):
           return userInput
       elif (userInput == "help" or userInput == "?" or userInput == "h"):
@@ -126,88 +142,92 @@ def displayHelp():
     printNow("You have choosen to display the Help Menu")
     printNow("")
     printNow("*** Welcome to The CSIT Guys\' House of Horrors! ***")
-    printNow("In each room you will be told which directions you can go")
-    printNow("You'll be able to go north, south, east or west by typing that direction")
-    printNow("Some rooms have items that can taken by typing 'take item' ")
-    printNow("Some rooms have closets that can be opened by typing 'open closet' ")
-    printNow("Type inventory or 'i' to display your inventory")
-    printNow("Items in inventory can be read by typing 'read' then the name of the item e.g. 'read book'")
-    printNow("Type help, h, or ? to redisplay this introduction")
-    printNow("Type exit to quit at any time")
-    userInput = ""
-    choice = userInput
-    return choice
+    printNow("In each room you will be told which directions you can go.")
+    printNow("You can to go north, south, east or west by typing that direction.")
+    printNow("Some rooms have items that can taken by typing 'take item'. ")
+    printNow("Some rooms have closets that can be opened by typing 'open closet'. ")
+    printNow("Type inventory or 'i' to display your inventory.")
+    printNow("Items in inventory can be read by typing 'read' then the name of the item e.g. 'read book'. ")
+    printNow("Type help, h, or ? to redisplay this introduction.")
+    printNow("Type exit to quit at any time.")
+ 
  
 def displayInventory(inventory):
     printNow("Inventory:")
     printNow(inventory) 
-       
+
+#Displays description for each room              
 def roomDescription(room):
     if room == "porch":
         printNow(room.upper())
-        printNow("You are on the porch of a haunted house")
-        printNow("You can go north (entryway) into the house ... if you dare")
-    if room == "entry way":
+        printNow("You are on the porch of a haunted house.")
+        printNow("You can go north (entryway) into the house ... if you dare.")
+    if room == "entryway":
         printNow(room.upper())
-        printNow("You are in the entryway of the house")
-        printNow("A grandfather clock chimes loudly as the door closes behind you")
-        printNow("You hear footsteps to the east")
-        printNow("There is a passageway to the north (kitchen), a door to the east (living room)")
-        printNow("The porch is behind you to the south")
+        printNow("You are in the entryway of the house.")
+        printNow("A grandfather clock chimes loudly as the door closes behind you.")
+        printNow("You hear distant, shambling footsteps to the east.")
+        printNow("There is a passageway to the north (kitchen), and a door to the east (living room).")
+        printNow("The porch is behind you to the south (porch).")
     if room == "dining room":
         printNow(room.upper())
-        printNow("You are in the dining room of the house")
-        printNow("In the center of the room is a dining room table that has been set, but no one sits")
-        printNow("There is passage to the east (kitchen))")
+        printNow("You are in the dining room of the house.")
+        printNow("In the center of the room is a dining room table that has been set, but no one sits.")
+        printNow("There is passage to the east (kitchen).")
     if room == "kitchen":
         printNow(room.upper())
-        printNow("There is a smell of burnt flesh")
+        printNow("You are in the kitchen.")
+        printNow("There is a smell of burnt flesh.")
         if "knife" in items: 
-          printNow("A knife rests on the counter, with a pig\'?s wet heart dripping into the sink")
+          printNow("A knife rests on the counter, with a pig\'Â?s wet heart dripping into the sink.")
         else:  
-          printNow("A pig\'?s wet heart is dripping into the sink")
-        printNow("On the table, a Martha Stewart catalog is sealed with dry blood")
-        printNow("A candlelight flickers from a cool breeze")
+          printNow("A pig\'Â?s wet heart is dripping into the sink.")
+        printNow("On the table, a Martha Stewart catalog is sealed shut with dried blood.")
+        printNow("A faint candlelight flickers from a cool breeze.")
         printNow("The is a door to the south (entryway) and a passage to the west (dining room)")
     if room == "living room":
         printNow(room.upper())
-        printNow("A rocking chair rocks to-and-fro, but no one sits upon it")
-        printNow("A mounted bear head seems to follow your every movement")
+        printNow("You are in the living room.")
+        printNow("A rocking chair rocks to-and-fro, as though it was just bumped heavily.")
+        printNow("A mounted bear head seems to follow your every movement.")
         if "book" in items:
-          printNow("A book is sitting on the end table")
-        printNow("The fireplace\'?s embers indicate it has been used recently")
-        printNow("There is a door to the west (entryway), a door to the north (bathroom), and a door to the east (bedroom)")
+          printNow("A book is sitting on the end table.")
+        printNow("The fireplace\'Â?s embers indicate it has been used recently.")
+        printNow("There is a door to the west (entryway), a door to the north (bathroom), and a door to the east (bedroom).")
     if room == "bathroom":
         printNow(room.upper())
-        printNow("You are staring at yourself through a water spotted mirror")
-        printNow("There is a medicine cabinet to your left. The sink has been used to wash the blood off something")
-        printNow("The toilet sits to the right of you and there is no toilet paper")
-        printNow("There is only one door to the south (living Room)")
+        printNow("You are in the bathroom.")
+        printNow("You are staring at yourself through a water spotted mirror.")
+        printNow("There is a medicine cabinet to your left. The sink has been used to wash the blood off something.")
+        printNow("The toilet sits to the right of you and there is no toilet paper.")
+        printNow("There is only one door to the south (living room).")
     if room == "bedroom":
         printNow(room.upper())
-        printNow("You walk into a dark and deserted bedroom")  
-        printNow("To your right there are weird noises coming from behind a closet door")
-        printNow("To your left underneath the bed there is some sort of bloody object")
-        printNow("There is only one door to the west (living Room), but you can open the closet")
+        printNow("You walk into a dark and deserted bedroom.")  
+        printNow("To your right there are weird noises coming from behind a closet door.")
+        printNow("To your left, underneath the bed, there is some sort of bloody object.")
+        printNow("There is only one door to the west (living room), but you can open the closet.")
     if room == "secret room":
         printNow(room.upper())
-        printNow("You found a secret room")
-        printNow("You look around, find a chest, and open the chest")
-        printNow("Within the chest are several dusty tomes, one of which you take out")
-        printNow("You blow the dust off the book")
-        printNow("The book is titled Programming in Python: A Magical Multimedia Experience")
+        printNow("You found a secret room!")
+        printNow("You look around and find a single large treasure chest.")
+        printNow("Within the chest are several dusty tomes, one of which you take out.")
+        printNow("You blow the dust off the book.")
+        printNow("The book is titled Programming in Python: A Magical Multimedia Experience.")
 
-
+#Function used to pick up items through out the game. Items are placed in the players inventory and removed from the available items in the game.
+#Arguments are the current room they are in and the item they have requested to pick up.
 def takeItem(room, item):
-    itemTaken = false
+    isItemTaken = false
     global inventory 
     if item in items:
       if items[item] == room:
         inventory = inventory + [item]
-        itemTaken= true 
+        isItemTaken= true 
         del items[item]   
-    return itemTaken
- 
+    return isItemTaken
+
+#Function used to read an item in inventory  
 def readItem(item): 
     global inventory
     if item in inventory:
@@ -221,13 +241,14 @@ def readItem(item):
       return true
     return false  
 
+#Function for opening the closet in the bedroom; if you have the knife you survive, otherwise you lose. 
 def openCloset():
     global inventory
     if "knife" in inventory: 
         printNow("A monster with a rusty nail bat lunges at you.")
         printNow("You use your knife and defend yourself, defeating the monster.")
         printNow("There appears to be another door in the closet. The monster was guarding something, perhaps.")
-        printNow("You can go east (Secret Room), through the door to the secret room.")
+        printNow("You can go east (secret room), through the door in the closet.")
         userInput = requestString("Choose a direction")
         userInput = parse(userInput)
         return userInput
@@ -238,11 +259,11 @@ def openCloset():
 def loseGame():
     printNow("A monster with a rusty nail bat lunges at you.")
     printNow("Weaponless, the monster easily bests you. It drags you into the darkness and begins to feast.")
-    
+
+#Called when user enters the secret room.         
 def winGame():
     roomDescription("secret room")
     printNow("Congratulations!")
     printNow("You just knew you would find something useful in this house")
     printNow("The End")
     return "exit"
-    
